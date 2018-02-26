816ae53c076fde5291ef4d219ce42f5fc03df15f
Drew Hubl <drew.hubl@gmail.com>
2018-02-25 19:08:41 -1000

    Split up tests into integration and unit tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -4,11 +4,13 @@ from django.test import TestCase, override_settings
 @override_settings(
     SECURE_MAIL_GNUPG_ENCODING=None)
 class GetGPGTestCase(TestCase):
-    def test_get_gpg(self):
+    def test_get_gpg_default_encoding(self):
         from secure_mail import utils
         previous_value = utils.GNUPG_ENCODING
         try:
             utils.GNUPG_ENCODING = None
-            utils.get_gpg()
+            gpg_obj = utils.get_gpg()
         finally:
             utils.GNUPG_ENCODING = previous_value
+
+        self.assertEquals(gpg_obj.encoding, 'latin-1')
