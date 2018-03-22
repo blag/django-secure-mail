f922a46d00b2509cc8622b8c1c1e47be213b185c
Drew Hubl <drew.hubl@gmail.com>
2018-03-22 13:24:40 -0700

    Test both the default GPG.encoding and a specified encoding

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,8 +1,6 @@
 from django.test import TestCase, override_settings
 
 
-@override_settings(
-    SECURE_MAIL_GNUPG_ENCODING=None)
 class GetGPGTestCase(TestCase):
     def test_get_gpg_default_encoding(self):
         from secure_mail import utils
@@ -13,4 +11,16 @@ class GetGPGTestCase(TestCase):
         finally:
             utils.GNUPG_ENCODING = previous_value
 
+        # GPG.encoding is hard-coded to latin-1 in gnupg.py
         self.assertEquals(gpg_obj.encoding, 'latin-1')
+
+    def test_get_gpg_specified_encoding(self):
+        from secure_mail import utils
+        previous_value = utils.GNUPG_ENCODING
+        try:
+            utils.GNUPG_ENCODING = 'utf-8'
+            gpg_obj = utils.get_gpg()
+        finally:
+            utils.GNUPG_ENCODING = previous_value
+
+        self.assertEquals(gpg_obj.encoding, 'utf-8')
